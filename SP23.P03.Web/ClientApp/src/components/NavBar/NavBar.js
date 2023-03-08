5f1db72a5c87679c198e48a5758f60acaa8638a5
NRanel <99266490+NRanel@users.noreply.github.com>
2023-03-08 13:31:50 -0600

    WIP Navbar

--- SP23.P03.Web/ClientApp/src/components/NavBar/NavBar.js
+++ SP23.P03.Web/ClientApp/src/components/NavBar/NavBar.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import './NavBar.css';
-
+import train from './train.svg';
 function NavBar() {
     return (
-        <>
-        
-        </>
+        <Navbar className="main" sticky="top">
+        <Container>
+          <Navbar.Brand href="#"> 
+            <img
+              alt="a train"
+              src={train}
+              width="50"
+              height="50"
+              className="d-inline-block align-center"
+            />
+          </Navbar.Brand>
+          <Navbar.Brand>
+            <i style={{color: 'white', fontSize: '50px', fontFamily: "'Nunito', sans-serif"}}>EnTrack</i>
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Placeholder</Nav.Link>
+            <Nav.Link>Placeholder</Nav.Link>
+            <Nav.Link>Placeholder</Nav.Link>
+            <Nav.Link>Placeholder</Nav.Link>
+            <Nav.Link>Placeholder</Nav.Link>
+            <Nav.Link>Placeholder</Nav.Link>
+            <Nav.Link>Placeholder</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     );
 }
 
