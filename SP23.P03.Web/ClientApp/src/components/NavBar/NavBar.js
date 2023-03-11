b908ec2859e95810e404d0f28264c33f0e1ed3a7
NRanel <99266490+NRanel@users.noreply.github.com>
2023-03-10 23:34:17 -0600

    Side menu

--- SP23.P03.Web/ClientApp/src/components/NavBar/NavBar.js
+++ SP23.P03.Web/ClientApp/src/components/NavBar/NavBar.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import Nav from 'react-bootstrap/Nav';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 import './NavBar.css';
 import train from './train.svg';
+import {useState} from 'react';
 function NavBar() {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return (
         <Navbar className="main" sticky="top">
         <Container>
           <Navbar.Brand href="#"> 
-            <img
+            <img onClick={handleShow}
               alt="a train"
               src={train}
               width="50"
@@ -17,18 +24,32 @@ function NavBar() {
               className="d-inline-block align-center"
             />
           </Navbar.Brand>
+          <Offcanvas show={show} onHide={handleClose}>
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title><b>Menu</b></Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav style={{fontSize: '30px'}}>
+              <Nav.Link>Train Stations</Nav.Link>
+              <Nav.Link>Routes</Nav.Link>
+              <Nav.Link>Trips</Nav.Link>
+              <Nav.Link>Tickets</Nav.Link>
+            </Nav>
+          </Offcanvas.Body>
+          </Offcanvas>
           <Navbar.Brand>
             <i style={{color: 'white', fontSize: '50px', fontFamily: "'Nunito', sans-serif"}}>EnTrack</i>
           </Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Placeholder</Nav.Link>
-            <Nav.Link>Placeholder</Nav.Link>
-            <Nav.Link>Placeholder</Nav.Link>
-            <Nav.Link>Placeholder</Nav.Link>
-            <Nav.Link>Placeholder</Nav.Link>
-            <Nav.Link>Placeholder</Nav.Link>
-            <Nav.Link>Placeholder</Nav.Link>
-          </Nav>
+          <Nav className="me-auto"/>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="light">Search</Button>
+          </Form>
         </Container>
       </Navbar>
     );
