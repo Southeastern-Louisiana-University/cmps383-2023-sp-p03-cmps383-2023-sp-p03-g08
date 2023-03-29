a0acf50ff84e28c2650756f69900a74f3abb54eb
Audrey Escarra <aescarra13@gmail.com>
2023-03-29 14:22:03 -0500

    completed controller

--- SP23.P03.Web/Controllers/PassengersController.cs
+++ SP23.P03.Web/Controllers/PassengersController.cs
@@ -99,6 +99,159 @@ namespace SP23.P03.Web.Controllers
 
             return Ok(ownedPassengers);
         }
+
+
+        [HttpPost]
+        [Authorize]
+        public async Task<ActionResult<PassengerDto>> CreatePassengerAsync([FromBody] CreatePassengerDto createPassengerDto)
+        {
+            var user = await User.GetCurrentUserAsync(userManager); 
+
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            if (InvalidCreatePassengerDto(createPassengerDto))
+            {
+                return BadRequest();
+            }
+
+            var createdPassenger = new Passenger
+            {
+                Owner = user,
+                FirstName = createPassengerDto.FirstName,
+                LastName = createPassengerDto.LastName,
+                Birthday = createPassengerDto.Birthday,
+            };
+
+            passengers.Add(createdPassenger);
+            dataContext.SaveChanges();
+
+            var passengerDto = new PassengerDto
+            {
+                Id = createdPassenger.Id,
+                OwnerId = createdPassenger.OwnerId,
+                FirstName = createdPassenger.FirstName,
+                LastName = createdPassenger.LastName,
+                Birthday = createdPassenger.Birthday,
+               
+            };
+
+            return Ok(passengerDto);
+        }
+
+        [HttpPost("for/{userId}")]
+        [Authorize(Roles = RoleNames.Admin)]
+        public async Task<ActionResult<PassengerDto>> CreatePassengerForAsync([FromBody] CreatePassengerDto createPassengerDto, [FromRoute] int userId)
+        {
+            var user = await userManager.FindByIdAsync(userId.ToString());
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            if (InvalidCreatePassengerDto(createPassengerDto))
+            {
+                return BadRequest();
+            }
+
+            var createdPassenger = new Passenger
+            {
+                Owner = user,
+                FirstName = createPassengerDto.FirstName,
+                LastName = createPassengerDto.LastName,
+                Birthday = createPassengerDto.Birthday,
+            };
+
+            passengers.Add(createdPassenger);
+            dataContext.SaveChanges();
+
+            var passengerDto = new PassengerDto
+            {
+                Id = createdPassenger.Id,
+                OwnerId = createdPassenger.OwnerId,
+                FirstName = createdPassenger.FirstName,
+                LastName = createdPassenger.LastName,
+                Birthday = createdPassenger.Birthday,
+               
+            };
+
+            return Ok(passengerDto);
+        }
+
+        [HttpPut("{id}")]
+        [Authorize]
+        public ActionResult<PassengerDto> UpdatePassenger([FromBody] CreatePassengerDto createPassengerDto,
+                                                          [FromRoute] int id)
+        {
+            var passenger = passengers.FirstOrDefault(x => x.Id == id);
+
+            if (passenger == null)
+            {
+                return NotFound();
+            }
+
+            if (!(User.IsInRole(RoleNames.Admin) || passenger.OwnerId == User.GetCurrentUserId()))
+            {
+                return Forbid();
+            }
+
+            if (InvalidCreatePassengerDto(createPassengerDto))
+            {
+                return BadRequest();
+            }
+
+            passenger.FirstName = createPassengerDto.FirstName;
+            passenger.LastName = createPassengerDto.LastName;
+            passenger.Birthday = createPassengerDto.Birthday;
+
+            dataContext.SaveChanges();
+
+            var passengerDto = new PassengerDto
+            {
+                Id = passenger.Id,
+                OwnerId = passenger.OwnerId,
+                FirstName = passenger.FirstName,
+                LastName = passenger.LastName,
+                Birthday = passenger.Birthday,
+                
+            };
+
+            return Ok(passengerDto);
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize]
+        public ActionResult DeletePassenger([FromRoute] int id)
+        {
+            var passenger = passengers.FirstOrDefault(x => x.Id == id);
+
+            if (passenger == null)
+            {
+                return NotFound();
+            }
+
+            if (!(User.IsInRole(RoleNames.Admin) || passenger.OwnerId == User.GetCurrentUserId()))
+            {
+                return Forbid();
+            }
+
+            passengers.Remove(passenger);
+            dataContext.SaveChanges();
+
+            return Ok();
+        }
+
+        private static bool InvalidCreatePassengerDto(CreatePassengerDto? createPassengerDto) =>
+            createPassengerDto == null
+            || String.IsNullOrWhiteSpace(createPassengerDto.FirstName)
+            || String.IsNullOrWhiteSpace(createPassengerDto.LastName)
+            || createPassengerDto.FirstName.Length > 64
+            || createPassengerDto.LastName.Length > 64
+            || DateTimeOffset.Now.AddYears(-120).CompareTo(createPassengerDto.Birthday) >= 0;
     }
+
 
 }
