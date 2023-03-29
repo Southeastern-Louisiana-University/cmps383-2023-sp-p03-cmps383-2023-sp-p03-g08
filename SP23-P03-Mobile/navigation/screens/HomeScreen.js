4648315a2af2dd6822cdcbc521c92216d6587e12
kchelle robinson <rkchelle64@gmail.com>
2023-03-29 17:52:40 -0500

    Adding buttons

--- SP23-P03-Mobile/navigation/screens/HomeScreen.js
+++ SP23-P03-Mobile/navigation/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 
 export default function HomeScreen({ navigation }) {
     return (
@@ -7,6 +7,7 @@ export default function HomeScreen({ navigation }) {
             <Text
                 onPress={() => alert('This is the "Home" screen.')}
                 style={styles.baseText}>Welcome to Entrack</Text>
+            <Button title="Book Ticket"></Button>
         </View>
     );
 }
