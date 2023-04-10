2a420cf2a566b89f5aa16c3dd83cd90f7523dc3e
NRanel <99266490+NRanel@users.noreply.github.com>
2023-04-10 11:48:25 -0500

    more train fields

--- SP23.P03.Web/Features/Trains/Train.cs
+++ SP23.P03.Web/Features/Trains/Train.cs
@@ -5,6 +5,10 @@ public class Train
 
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public int Capacity { get; set; }
+    public int CoachCapacity { get; set; }
+    public int FirstClassCapacity { get; set; }
+    public int SleeperCapacity { get; set; }
+    public int RoomletCapacity { get; set; }
+    public Boolean Dining { get; set; }
 
 }
