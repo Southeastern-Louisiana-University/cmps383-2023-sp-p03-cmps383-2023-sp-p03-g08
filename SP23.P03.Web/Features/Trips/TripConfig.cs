93a1f500d5e8f78858bb3fdc79ab47ffd710d5f8
NRanel <99266490+NRanel@users.noreply.github.com>
2023-04-12 10:58:16 -0500

    WIP Trip

---
+++ SP23.P03.Web/Features/Trips/TripConfig.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SP23.P03.Web.Features.Trips;
+
+public class TripConfig : IEntityTypeConfiguration<Trip>
+{
+    public void Configure(EntityTypeBuilder<Trip> builder)
+    {
+        builder.Property(x => x.TrainId).IsRequired();
+        builder.Property(x => x.RouteId).IsRequired();
+        builder.Property(x => x.CoachSeatsLeft).IsRequired();
+        builder.Property(x => x.CoachPrice).IsRequired();
+        builder.Property(x => x.FirstClassSeatsLeft).IsRequired();
+        builder.Property(x => x.FirstClassPrice).IsRequired();
+        builder.Property(x => x.SleepersLeft).IsRequired();
+        builder.Property(x => x.SleeperPrice).IsRequired();
+        builder.Property(x => x.RoomletsLeft).IsRequired();
+        builder.Property(x => x.RoomletsPrice).IsRequired();
+        builder.Property(x => x.Dining).IsRequired();
+        builder.Property(x => x.BasePrice).IsRequired();
+    }
+}
---
+++ SP23.P03.Web/Features/Trips/TripDto.cs
@@ -0,0 +1,20 @@
+﻿using SP23.P03.Web.Features.Trains;
+
+namespace SP23.P03.Web.Features.Trips;
+
+public class TripDto
+{
+    public int Id { get; set; }
+    public int TrainId { get; set; }
+    public int RouteId { get; set; }
+    public int CoachSeatsLeft { get; set; }
+    public decimal CoachPrice { get; set; }
+    public int FirstClassSeatsLeft { get; set; }
+    public decimal FirstClassPrice { get; set; }
+    public int SleepersLeft { get; set; }
+    public decimal SleeperPrice { get; set; }
+    public int RoomletsLeft { get; set; }
+    public decimal RoomletsPrice { get; set; }
+    public Boolean Dining { get; set; }
+    public decimal BasePrice { get; set; }
+}
