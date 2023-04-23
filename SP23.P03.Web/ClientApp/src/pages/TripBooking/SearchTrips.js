46ba9e984a69dadb94ff9e65c2ed533ca25427ba
NRanel <99266490+NRanel@users.noreply.github.com>
2023-04-23 16:19:34 -0500

    map user tickets on their profile

--- SP23.P03.Web/ClientApp/src/pages/TripBooking/SearchTrips.js
+++ SP23.P03.Web/ClientApp/src/pages/TripBooking/SearchTrips.js
@@ -9,7 +9,6 @@ import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Accordion from 'react-bootstrap/Accordion';
 import Badge from 'react-bootstrap/Badge';
-import { useState } from "react";
 //import Modal from "react-bootstrap/Modal";
 import BookingModal from "./BookingModal";
 function SearchTrips({searchtrips, deploc, depdate, arrloc, arrdate}) {
@@ -36,8 +35,7 @@ function SearchTrips({searchtrips, deploc, depdate, arrloc, arrdate}) {
         }
         return ts;
     }
-    var seat;
-    var price;
+    
     return (
         <>
         <Container>
